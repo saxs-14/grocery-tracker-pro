0bae462092fde68dd2acfae42b2ca7179e69b959
Phathutshedzo Mamagau <mamagauphathutshedzosaxs@gmail.com>
2026-09-23 23:01:30 +0200

    Upgrade server/routes/reportRoutes.js for real user-scoped data and validation

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getCategoryStats, getMonthlyStats } = require('../controllers/reportController');
+const { protect } = require('../middleware/authMiddleware');
+const { getCategoryStats, getMonthlyStats, getSummary } = require('../controllers/reportController');
 
+router.use(protect);
 router.get('/category-stats', getCategoryStats);
 router.get('/monthly-stats', getMonthlyStats);
+router.get('/summary', getSummary);
 
 module.exports = router;
